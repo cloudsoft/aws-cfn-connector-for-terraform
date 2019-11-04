6f64cff6c4188fbc3bfa975e020f70ea6b4786c8
Denis Ovsienko <denis@cloudsoft.io>
2019-11-04 18:04:10 +0000

    cloudsoft-terraform-template: save sshj WIP
    This is the first proof of concept made under Thomas' guidance.
    
    $ mvn test
    [...]
    [INFO] Running io.cloudsoft.terraform.template.CreateHandlerTest
    SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
    SLF4J: Defaulting to no-operation (NOP) logger implementation
    SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
    Linux cloudsoft-denis 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
    
    [INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.967 s - in io.cloudsoft.terraform.template.CreateHandlerTest

--- cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/CreateHandler.java
+++ cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/CreateHandler.java
@@ -2,35 +2,20 @@ package io.cloudsoft.terraform.template;
 
 import com.amazonaws.cloudformation.proxy.AmazonWebServicesClientProxy;
 import com.amazonaws.cloudformation.proxy.Logger;
-import com.amazonaws.cloudformation.proxy.OperationStatus;
 import com.amazonaws.cloudformation.proxy.ProgressEvent;
+import com.amazonaws.cloudformation.proxy.OperationStatus;
 import com.amazonaws.cloudformation.proxy.ResourceHandlerRequest;
-import com.amazonaws.services.logs.AWSLogs;
-import com.amazonaws.services.logs.AWSLogsClientBuilder;
-import com.amazonaws.services.logs.model.MetricTransformation;
-import com.amazonaws.services.logs.model.PutMetricFilterRequest;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class CreateHandler extends BaseHandler<CallbackContext> {
-
-    private List<MetricTransformation> getMetricTransformations(final ResourceModel model) {
-        List<MetricTransformationProperty> metricTransformations =
-                model.getMetricTransformations();
-        if (metricTransformations == null) {
-            return Collections.emptyList();
-        }
-        return metricTransformations.stream()
-                .map(mtp -> new MetricTransformation()
-                        .withMetricName(mtp.getMetricName())
-                        .withMetricValue(mtp.getMetricValue())
-                        .withMetricNamespace(mtp.getMetricNamespace())
-                        .withDefaultValue(mtp.getDefaultValue()))
-                .collect(Collectors.toList());
-    }
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
@@ -38,25 +23,47 @@ public class CreateHandler extends BaseHandler<CallbackContext> {
             final ResourceHandlerRequest<ResourceModel> request,
             final CallbackContext callbackContext,
             final Logger logger) {
-
         ResourceModel model = request.getDesiredResourceState();
+        String s1, s2;
+        OperationStatus ret = OperationStatus.PENDING;
 
-        // Convert model to a form that works with your API
-        PutMetricFilterRequest putMetricFilterRequest = new PutMetricFilterRequest()
-                .withLogGroupName(model.getLogGroupName())
-                .withFilterPattern(model.getFilterPattern())
-                .withMetricTransformations(getMetricTransformations(model))
-                .withFilterName(model.getFilterName());
+        try {
+            final SSHClient ssh = new SSHClient();
+            // FIXME: keep the fingerprint(s) in an external state instead of hardcoding
+            ssh.loadKnownHosts(); // has no effect even when run on the dev PC
+            ssh.addHostKeyVerifier("3a:87:1f:a6:d8:6a:32:b7:47:fe:2d:e1:16:3a:bc:38");
+            ssh.connect("localhost");
+            Session session = null;
+            try {
+                ssh.authPublickey("denis", "src/main/resources/id_rsa_java");
+                // ssh.authPublickey("denis", "/home/denis/.ssh/id_rsa"); // does not work (agent?)
+                session = ssh.startSession();
+                final Command cmd = session.exec("uname -a");
+                s1 = IOUtils.readFully(cmd.getInputStream()).toString();
+                cmd.join(5, TimeUnit.SECONDS);
+                s2 = "exit status: " + cmd.getExitStatus(); // TBD
+                // cmd.getErrorStream() // TDB
+                //model.setMetricValue(s1); // does not compile
+                System.out.println(s1);
+            } finally {
+                try {
+                    if (session != null) {
+                        session.close();
+                    }
+                } catch (IOException e) {
+                    // do nothing
+                }
 
-        // Initialize client and send request
-        AWSLogs client = AWSLogsClientBuilder.standard().withRegion("eu-central-1").build();
-        // This is a minimal example, so we've kept it easy with no error handling
-        // You should add more error handling in real-world handlers
-        proxy.injectCredentialsAndInvoke(putMetricFilterRequest,
-                client::putMetricFilter);
+                ssh.disconnect();
+            }
+            ret = OperationStatus.SUCCESS;
+
+        } catch (IOException e) {
+            ret = OperationStatus.FAILED;
+        }
         return ProgressEvent.<ResourceModel, CallbackContext>builder()
                 .resourceModel(model)
-                .status(OperationStatus.SUCCESS)
+                .status(ret)
                 .build();
     }
 }
