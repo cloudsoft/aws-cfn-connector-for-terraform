3468b08fb0972c2a3ab1384802b2cb229bbb9954
Denis Ovsienko <denis@cloudsoft.io>
2019-11-08 12:17:06 +0000

    add a Configuration::resourceDefinedTags() placeholder

--- cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/Configuration.java
+++ cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/Configuration.java
@@ -1,6 +1,7 @@
 package io.cloudsoft.terraform.template;
 
 import java.io.InputStream;
+import java.util.Map;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -15,4 +16,10 @@ class Configuration extends BaseConfiguration {
         return new JSONObject(new JSONTokener(this.getClass().getClassLoader().getResourceAsStream(schemaFilename)));
     }
 
+    /**
+     * Providers should implement this method if their resource has a 'Tags' property to define resource-level tags * @return
+     */
+    public Map<String, String> resourceDefinedTags(final ResourceModel resourceModel) {
+        return null;
+    }
 }
