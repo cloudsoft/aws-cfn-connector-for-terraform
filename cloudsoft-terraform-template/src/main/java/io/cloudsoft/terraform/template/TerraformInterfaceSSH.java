66067068544c21696934cd646f2c0bc4ad8cf70a
Denis Ovsienko <denis@cloudsoft.io>
2019-11-12 13:59:47 +0000

    drop BuiltinKeyFile and hard-coded SSH parameters

--- cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/TerraformInterfaceSSH.java
+++ cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/TerraformInterfaceSSH.java
@@ -24,34 +24,6 @@ class TerraformInterfaceSSH {
         this.templateName = templateName;
     }
     
-    TerraformInterfaceSSH(String serverHostname, String templateName) {
-        // FIXME: fetch parameters from a state
-        switch (serverHostname)
-        {
-            case "localhost":
-                // In a Docker container loopback is in its own namespace, the client has to reach the host.
-                this.serverHostname = "192.168.101.123";
-                this.sshServerKeyFP = "3a:87:1f:a6:d8:6a:32:b7:47:fe:2d:e1:16:3a:bc:38";
-                this.sshUsername = "terraform";
-                // The key file ends up at the root level of the JAR, hence pay attention to the path
-                // to keep the code working in a lambda environment (in particular, SAM local tests
-                // depend on this).
-                this.sshClientSecretKeyFile = "id_rsa_java";
-                break;
-            case "tf-denis":
-                this.serverHostname = "ec2-54-93-230-94.eu-central-1.compute.amazonaws.com";
-                this.sshServerKeyFP = "bb:4e:e3:73:76:a0:4e:bc:58:7a:d5:6c:0d:a8:f8:12";
-                this.sshUsername = "ubuntu";
-                // Idem.
-                this.sshClientSecretKeyFile = "terraform-denis-20191104.pem";
-                break;
-            default:
-                throw new IllegalArgumentException ("Unknown Terraform server name '" + serverHostname + "'");
-        }
-        sshClientSecretKeyContents = null;
-        this.templateName = templateName;
-    }
-
     void createTemplateFromURL(String url) throws IOException {
         runSSHCommand(String.format ("mkdir -p ~/tfdata/'%s'", templateName));
         runSSHCommand(String.format ("cd ~/tfdata/'%s' && wget --output-document=configuration.tf '%s'", templateName, url));
@@ -82,24 +54,11 @@ class TerraformInterfaceSSH {
 
         final SSHClient ssh = new SSHClient();
 
-        // FIXME: keep the fingerprint(s) in an external state instead of hardcoding
-        // loadKnownHosts() has no effect even when run on the dev PC
         ssh.addHostKeyVerifier(sshServerKeyFP);
-
         ssh.connect(serverHostname);
         Session session = null;
         try {
-
-            // src/main/resources/privkey works.
-            // /home/user/.ssh/privkey works if the file has no passphrase (sshj does
-            // not support SSH agent, and there is no SSH agent in AWS anyway).
-            // ~/.ssh/privkey does not work.
-            // ~user/.ssh/privkey does not work.
-            ssh.authPublickey(sshUsername, 
-                getKeyProvider()
-//                new BuiltinKeyFile(sshClientSecretKeyFile)
-                );
-
+            ssh.authPublickey(sshUsername, getKeyProvider());
             session = ssh.startSession();
             final Session.Command cmd = session.exec(command);
             String s1 = IOUtils.readFully(cmd.getInputStream()).toString();
