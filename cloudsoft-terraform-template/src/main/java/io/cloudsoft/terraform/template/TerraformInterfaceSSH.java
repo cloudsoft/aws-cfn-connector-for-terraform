e9cca75a53ba3c6795c56f1e474f1e0128959405
Denis Ovsienko <denis@cloudsoft.io>
2019-11-15 19:08:14 +0000

    use SCP to upload TF configuration to the server
    * TerraformBaseHandler: Make getConfiguration() public so the workers
      can actually use it.
    * TerraformInterfaceSSH: Make and comment directory and file name
      workarounds so the sshj implementation of SCP does not break. Add
      uploadConfiguration() for SCP transfers and a small helper class.
    * AbstractHandlerWorker: Add a convenience getAndUploadConfiguration()
      method to upload whatever the handler considers right with SCP.
    * CreateHandlerWorker: Use the method and rename the FSM state for
      consistency.
    * UpdateHandlerWorker: idem

--- cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/TerraformInterfaceSSH.java
+++ cloudsoft-terraform-template/src/main/java/io/cloudsoft/terraform/template/TerraformInterfaceSSH.java
@@ -4,8 +4,11 @@ import com.amazonaws.cloudformation.proxy.AmazonWebServicesClientProxy;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.xfer.InMemorySourceFile;
+import software.amazon.awssdk.utils.StringInputStream;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 public class TerraformInterfaceSSH {
@@ -18,7 +21,12 @@ public class TerraformInterfaceSSH {
     // useful after parameters can be specified separately for each server).
     private static final String
             // TF_DATADIR must match the contents of the files in server-side-systemd/
-            TF_DATADIR = "~/tfdata",
+            // (at least as far as realpath(1) is concerned).
+            // sshj does not expand tilde to the remote user's home directory on the server
+            // (OpenSSH scp does that). Also neither any directory components nor the
+            // file name can be quoted (as in "/some/'work dir'/otherdir") because sshj
+            // fails to escape the quotes properly (again, works in OpenSSH).
+            TF_DATADIR = "/home/ubuntu/tfdata",
             TF_CONFFILENAME = "configuration.tf";
 
     public TerraformInterfaceSSH(TerraformBaseHandler<?> h, AmazonWebServicesClientProxy proxy, String templateName) {
@@ -30,8 +38,8 @@ public class TerraformInterfaceSSH {
         this.templateName = templateName;
     }
 
-    protected String getWorkdir() {
-        return String.format("%s/'%s'", TF_DATADIR, templateName);
+    private String getWorkdir() {
+        return String.format("%s/%s", TF_DATADIR, templateName);
     }
 
     public void onlyMkdir() throws IOException {
@@ -74,6 +82,40 @@ public class TerraformInterfaceSSH {
                 // do nothing
             }
             ssh.disconnect();
+        }
+    }
+
+    public void uploadConfiguration (String contents) throws IOException {
+        StringSourceFile src = new StringSourceFile(TF_CONFFILENAME, contents);
+        SSHClient ssh = new SSHClient();
+        ssh.addHostKeyVerifier(sshServerKeyFP);
+        ssh.connect(serverHostname, sshPort);
+        try {
+            ssh.authPublickey(sshUsername, new SSHClient().loadKeys(sshClientSecretKeyContents, null, null));
+            ssh.newSCPFileTransfer().upload(src, getWorkdir());
+        } finally {
+            ssh.disconnect();
+        }
+    }
+
+    private class StringSourceFile extends InMemorySourceFile {
+        private String name, contents;
+
+        StringSourceFile (String name, String contents){
+            this.name = name;
+            this.contents = contents;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getLength() {
+            return contents.length();
+        }
+
+        public InputStream getInputStream() {
+            return new StringInputStream(contents);
         }
     }
 }
